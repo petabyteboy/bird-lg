4be9730746749d89663af8b34ae8f2fae65a3fe6
TimStallard <code@timstallard.me.uk>
2019-01-27 15:36:01 +0000

    Exclude BFD protos

--- lg.py
+++ lg.py
@@ -232,7 +232,7 @@ def whois():
     return jsonify(output=output, title=query)
 
 
-SUMMARY_UNWANTED_PROTOS = ["Kernel", "Static", "Device"]
+SUMMARY_UNWANTED_PROTOS = ["Kernel", "Static", "Device", "BFD"]
 
 @app.route("/summary/<hosts>")
 @app.route("/summary/<hosts>/<proto>")
